dfde436cc3376b7b5656926ec6ee781eaee2c061
faridulhaque <faridulhaquemurshed@gmail.com>
2023-03-08 21:42:40 +0600

    model updae

--- services/models/QuestionPaper.model.js
+++ services/models/QuestionPaper.model.js
@@ -2,38 +2,56 @@ import mongoose from "mongoose";
 import QuestionsSchema from "./questions.model.js";
 
 const QuestionPaperSchema = new mongoose.Schema({
-    moderator: {
+  moderator: {
+    type: String,
+    required: true,
+    min: 5,
+    max: 50,
+  },
+  status: {
+    type: String,
+    default: "Incomplete",
+  },
+  title: {
+    type: String,
+    required: true,
+    min: 1,
+    max: 100,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 5,
+    max: 40,
+  },
+  attendance: {
+    type: [{
+      attendant: {
         type: String,
-        required: true,
-        min: 5,
-        max: 50
-    },
-    title: {
-        type: String,
-        required: true,
-        min: 1,
-        max: 100,
-    },
-    quantity: {
-        type: Number,
-        required: true,
-        min: 5,
-        max: 40,
-    },
-    questions: {
-        type: [QuestionsSchema],
-        default: [],
-    },
-
-    duration: String,
-    start: String,
-
-
-}, { timestamps: true })
-
+        default: ""
+      },
+      present: {
+        type: Boolean,
+        default: false
+      }
+    }],
+    default: []
+  },
+  questions: {
+    type: [QuestionsSchema],
+    default: [],
+  },
+  duration: {
+    type: Object,
+    default: { hour: 0, min: 0, sec: 0 },
+  },
+  start: {
+    type: Date,
+    required: true,
+    
+  },
+}, { timestamps: true });
 
 const questionPaperModel = mongoose.model("QuestionPaper", QuestionPaperSchema);
 
 export default questionPaperModel;
-
-
